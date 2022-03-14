e115599ae83fe15b63a493e272ba8b9d3baf421d
Sean Kan <59797882+sclkan@users.noreply.github.com>
2022-03-15 01:11:36 +0900

    Added comments

--- src/shortestPath/Pair.java
+++ src/shortestPath/Pair.java
@@ -1,12 +1,27 @@
 package shortestPath;
 
+/**
+ * Pair data structure for creating an adjacency list
+ * @param <E>  Generic object
+ * @param <F>  Generic object
+ */
 public class Pair<E, F>
 {
     public E first;
     public F second;
 
+    /**
+     * Constructor
+     * @param x   Generic object (vertex in this case)
+     * @param y  Generic object  (cost in this case)
+     */
     public Pair(E x, F y) { first = x; second = y; }
 
+    /**
+     * Compares two objects
+     * @param rhs   Object to compare with
+     * @return   True if identical
+     */
     public boolean equals(Object rhs)
     {
         Pair<E, F> other;
@@ -14,6 +29,10 @@ public class Pair<E, F>
         return first.equals(other.first);
     }
 
+    /**
+     * Obtain hashcode value of an object
+     * @return  Hashcode
+     */
     public int hashCode()
     {
         return first.hashCode();
